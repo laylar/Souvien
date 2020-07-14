72a3a246b7567ea1060873014a44cef07d7a5fbb
Layla <layla.rowen@gmail.com>
2020-07-13 18:41:55 -0700

    Updated HomeComponent placeholder content

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -8,9 +8,26 @@ class Home extends Component {
     }
     render() {
         return (
-            <div>
-                <p>this is where some home text will go</p>
+            <div className="container">
+            <div className="row">
+                <div className="col">
+                    <hr />
+                </div>
             </div>
+            <div className="row row-content">
+                <div>
+                    <h3>Home Sweet Home</h3>
+                    <p>This is where some welcome home text will go</p>
+                    <p>There will be a way to log in.</p> 
+                    <p> 
+                        When logged in, 
+                        Home will be where you see the quote associated with 
+                        the date and you can add your journal entries!
+                    </p>
+                </div>
+            </div>
+
+        </div>
         );
     }
 }
