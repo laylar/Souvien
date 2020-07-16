49f3b33a4c4e09fba4153f55ebfc454a6788158a
Layla <layla.rowen@gmail.com>
2020-07-15 21:44:11 -0700

    planning text on Homepage

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -18,12 +18,22 @@ class Home extends Component {
                     <div className="col">
                         <h3>Home Sweet Home</h3>
                         <p>This is where some welcome home text will go</p>
-                        <p>There will be a way to log in.</p>
                         <p>
-                            When logged in,
-                            Home will be where you see the quote associated with
-                            the date and you can add your journal entries!
-                    </p>
+                            <b>When not logged in this page will show a login page.</b>
+                        </p>
+                        <p>
+                            <b>When logged in: </b>
+                        </p>
+                        <p>
+                            A Textbox for entering journal entry for the day will go here...
+                            ...next to the entry box there will be a quote associated with the date
+                        </p>
+                        <p><i>The section above this will be stuck to the top. The entries below can scroll.</i></p>
+                        <p>
+                            all the previous entries on this date go here...
+                            ...and next to that is a calendar to select other dates with a search box underneath
+                        </p>
+
                     </div>
                 </div>
 
