56815694fc6401e7f09575062f882f36513afaa0
Layla <layla.rowen@gmail.com>
2020-07-19 14:06:56 -0700

    #12 calendar displayed via react-calendar

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,33 +1,52 @@
-import React from 'react';
-import {Card, CardTitle, CardText} from 'reactstrap';
+import React, {Component} from 'react';
+import {Card, CardTitle, CardText, Form, FormGroup, Label, Input, Button} from 'reactstrap';
+import Calendar from 'react-calendar';
 
+class CurrDate extends Component {
+//More info here: https://www.npmjs.com/package/react-calendar
+//Examples here: https://github.com/wojtekmaj/react-calendar/wiki/Recipes
+    state = {
+        date: new Date(),
+      }
+     
+      onChange = date => this.setState({ date })
+     
+      render() {
+        return (
+          <div>
+            <Calendar
+              onChange={this.onChange}
+              value={this.state.date}
+            />
+          </div>
+        );
+      }
+    }
 
 function Home (props) {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md m-1">
+                <div className="col-md-9">
                     <Card>
-                        <CardTitle>Entry Date: July 19th, 2020</CardTitle>
-                        <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</CardText>
+                        <Form>
+                            <FormGroup>
+                                <Label for="entry-data"></Label>
+                                <Input type="textarea" name="newentry" id="entry-data" />
+                            </FormGroup>
+                            <Button>Save The Day</Button>
+                        </Form>
                     </Card>
-
                 </div>
+                <div className="col-md-3">
+                    <CurrDate />
+                </div>
+                
             </div>
             <div className="row row-content">
                 <div className="col">
-                    <h3>Home Sweet Home</h3>
-                    <p>This is where some welcome home text will go</p>
                     <p>
                         <b>When not logged in this page will show a login page.</b>
-                    </p>
-                    <p>
-                        <b>When logged in: </b>
-                    </p>
-                    <p>
-                        A Textbox for entering journal entry for the day will go here...
-                        ...next to the entry box there will be a quote associated with the date
                     </p>
                     <p><i>The section above this will be stuck to the top. The entries below can scroll.</i></p>
                     <Card>
