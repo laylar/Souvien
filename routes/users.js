729a69112e0ca6c4053669a06d1e8e6aabaa9b2a
Layla <layla.rowen@gmail.com>
2020-07-18 17:16:15 -0700

    updated file names

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,50 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const userRouter = express.Router();
 
-module.exports = router;
+userRouter.use(bodyParser.json());
+
+userRouter.route('/')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res) => {
+        res.end('Will send all the users to you');
+    })
+    .post((req, res) => {
+        res.end(`Will add the user: ${req.body.name} with description: ${req.body.description}`);
+    })
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /users');
+    })
+    .delete((req, res) => {
+        res.end('Deleting all users');
+    });
+
+//still not sure about how I want this router to work. I want to have user info, as well as user settings...
+//but for now, it is a router just like the other two.
+userRouter.route('/:userId')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res) => {
+        res.end(`Will send details of user ${req.params.userId} to you`);
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /users/${req.params.userId}`);
+    })
+    .put((req, res) => {
+        res.write(`Updating the user: ${req.params.userId}\n`);
+        res.end(`Will update the user: ${req.body.name}
+        with description: ${req.body.description}`);
+    })
+    .delete((req, res) => {
+        res.end(`Deleting user: ${req.params.userId}`);
+    });
