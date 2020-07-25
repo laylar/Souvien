c983cc3cbfbb5a75c2e2b91b72bff2cabde9714f
Layla <layla.rowen@gmail.com>
2020-07-24 18:20:53 -0700

    #10 vanilla dummy Entries are now reachable by Express Server!

---
+++ models/entry.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+//const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const entrySchema = new Schema({
+  day: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+//planning to use Passport for user logins
+//userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("Entry", entrySchema);
---
+++ routes/entries.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Entry = require("../models/entry");
+const cors = require("./cors");
+
+const entriesRouter = express.Router();
+
+entriesRouter.use(bodyParser.json());
+
+entriesRouter
+  .route("/")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, (req, res, next) => {
+    Entry.find()
+      .then((entries) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(entries);
+      })
+      .catch((err) => next(err));
+    //   })
+    //   .post(cors.corsWithOptions, (req, res, next) => {
+    //     Entry.create(req.body)
+    //       .then((entry) => {
+    //         console.log("Entry added: ", entry);
+    //         res.statusCode = 200;
+    //         res.setHeader("Content-Type", "application/json");
+    //         res.json(entry);
+    //       })
+    //       .catch((err) => next(err));
+    //   })
+    //   .put(cors.corsWithOptions, (req, res) => {
+    //     res.statusCode = 403;
+    //     res.end("PUT operation not supported on /entries");
+    //   })
+    //   .delete(cors.corsWithOptions, (req, res) => {
+    //     Entry.deleteMany()
+    //       .then((response) => {
+    //         res.statusCode = 200;
+    //         res.setHeader("Content-Type", "application/json");
+    //         res.json(response);
+    //       })
+    //       .catch((err) => next(err));
+  });
+
+entriesRouter
+  .route("/:entryId")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, (req, res, next) => {
+    Entry.findById(req.params.entryId)
+      .then((entry) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(entry);
+      })
+      .catch((err) => next(err));
+    //   })
+    //   .post(cors.corsWithOptions, (req, res) => {
+    //     res.statusCode = 403;
+    //     res.end(`POST operation not supported on /entries/${req.params.entryId}`);
+    //   })
+    //   .put(cors.corsWithOptions, (req, res, next) => {
+    //     Entry.findByIdAndUpdate(
+    //       req.params.entryId,
+    //       {
+    //         $set: req.body,
+    //       },
+    //       { new: true }
+    //     )
+    //       .then((response) => {
+    //         res.statusCode = 200;
+    //         res.setHeader("Content-Type", "application/json");
+    //         res.json(response);
+    //       })
+    //       .catch((err) => next(err));
+    //   })
+    //   .delete(cors.corsWithOptions, (req, res) => {
+    //     res.statusCode = 403;
+    //     res.end(`DELETE operation not supported on /entries/${req.params.entryId}`);
+  });
+
+module.exports = entriesRouter;
